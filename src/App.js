fe1ef0302d732b0bf08154677844a67a3a6c5ed7
meteoben <meteoben@gmail.com>
2024-01-19 11:07:58 +0100

    commit ok backend-front

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React from "react"
 
-class App extends React.Component {
-  state = {
-    name: ""
+// class App extends React.Component {
+//   state = {
+//     name: ""
+//   }
+
+//   componentDidMount() {
+//     fetch("http://localhost:3000")
+//       .then(res => res.json())
+//       .then(data => this.setState({ name: data.name }))
+//   }
+
+//   render() {
+//     return (
+//       <h1>Hello {this.state.name}!</h1>
+//     )
+//   }
+// }
+
+// export default App
+
+
+
+const apiCall = () => {
+//   axios.get('http://localhost:8080').then((data) => {
+//     //this console.log will be in our frontend console
+//     console.log(data)
+//   })
+    fetch("http://localhost:8080")
+      // .then(res => res.json())
+      // .then(data => this.setState({ name: data.name }))
+      .then(data => {
+      console.log(data)
+    })
   }
 
-  componentDidMount() {
-    fetch("http://localhost:3000")
-      .then(res => res.json())
-      .then(data => this.setState({ name: data.name }))
-  }
+ export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
 
-  render() {
-    return (
-      <h1>Hello {this.state.name}!</h1>
-    )
-  }
+        <button onClick={apiCall}>Make API Call</button>
+
+      </header>
+    </div>
+  );
 }
-
-export default App
