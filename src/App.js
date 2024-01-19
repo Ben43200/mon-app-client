658bb49f83225f8aee5dd746a208b7c50600fa43
meteoben <meteoben@gmail.com>
2024-01-19 16:13:36 +0100

    mise en place du frontend pour users

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 // class App extends React.Component {
 //   state = {
@@ -22,27 +22,44 @@ import React from "react"
 
 
 
-const apiCall = () => {
-//   axios.get('http://localhost:8080').then((data) => {
-//     //this console.log will be in our frontend console
-//     console.log(data)
-//   })
-    fetch("https://mon-app-api-production.up.railway.app")
-      // .then(res => res.json())
-      // .then(data => this.setState({ name: data.name }))
-      .then(data => {
-      console.log(data)
-    })
-  }
+// const apiCall = () => {
+// //   axios.get('http://localhost:8080').then((data) => {
+// //     //this console.log will be in our frontend console
+// //     console.log(data)
+// //   })
+//     fetch("https://mon-app-api-production.up.railway.app")
+//       // .then(res => res.json())
+//       // .then(data => this.setState({ name: data.name }))
+//       .then(data => {
+//       console.log(data)
+//     })
+//   }
 
  export default function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+useEffect(() =>{
+fetch("/api").then(
+  response => response.json()
+).then(
+  data => {
+    setBackendData(data)
+  }
+)
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-
-        <button onClick={apiCall}>Make API Call</button>
-
-      </header>
+     {(typeof backendData.users === 'undefined') ?(
+      <p>Loading</p>
+     ) : (
+      backendData.users.map((user, i) => (
+        <p key={i}>{user}</p>
+      ))
+     )}
     </div>
   );
 }
+
+
+
